dedf0ab0d2a148e3df20688f7205ddb96bf62ced
marota <antoine-marot@live.fr>
2022-09-14 18:00:21 +0200

    demonstration of computing virtual flows with thetas and equivalent resistance matrix

---
+++ superposition_theorem.py
@@ -0,0 +1,168 @@
+import grid2op
+import numpy as np
+from lightsim2grid import LightSimBackend
+
+
+#so pVirtual_l1=por_Lconnected_l1+(DeltaPVirtual_l2*LODF2->1+DeltaPVirtual_l1*LODF3->1)
+#por_l2=LODF1->2*pVirtual1=LODF1->2*(por_Lconnected_l1+(DeltaPVirtual_l2*LODF2->1+DeltaPVirtual_l3*LODF3->1))
+#por_l3=LODF1->3*pVirtual1=LODF1->3*(por_Lconnected_l1+(DeltaPVirtual_l2*LODF2->1+DeltaPVirtual_l1*LODF3->1))
+
+#(por_l2-LODF3->2*DeltaPVirtual_l3)-DeltaPVirtual_l2=0
+#(LODF1->2*(por_Lconnected_l1+(DeltaPVirtual_l2*LODF2->1+DeltaPVirtual_l3*LODF3->1))-LODF3->2*DeltaPVirtual_l3)-DeltaPVirtual_l2=0
+#LODF1->2*por_Lconnected_l1+(LODF1->2*LODF2->1-1)*DeltaPVirtual_l2+(LODF1->2*LODF3->1-LODF3->2)*DeltaPVirtual_l3=0
+
+#LODF1->3*por_Lconnected_l1+(LODF1->3*LODF2->1-LODF2->3)*DeltaPVirtual_l2+(LODF1->3*LODF3->1-1)*DeltaPVirtual_l3=0
+
+#a generic version for n-K
+def get_DeltaVirtual_Flows_NK(il_connect,p_il_connect,A,ilds):
+    
+    
+    a=[]
+    for idl in ilds:
+        a_row=np.array([A[il_connect][idl]*A[idlj][il_connect]+A[idlj][idl] for idlj in ilds])
+        a.append(a_row)
+
+    b=np.array([-p_il_connect*A[il_connect][idl] for idl in ilds])
+    pls_virtual=np.linalg.solve(a,b)
+    print(pls_virtual)
+    
+    por_virtual_il_connect=p_il_connect
+    for i in range(len(ilds)):
+        por_virtual_il_connect+=A[ilds[i]][il_connect]*pls_virtual[i]
+    
+    return por_virtual_il_connect
+
+
+def get_Flows_NPlusK(p_init,A,ilds,p_ilds_connect):
+    
+    nl_connect=len(p_ilds_connect)
+    por_virtual_ilds=[]
+    for idx,il_connect in enumerate(ilds):
+        p_il_connect=p_ilds_connect[idx]
+        ilds_il_connect=[idl for idl in ilds if idl!=il_connect]
+        por_virtual_ilds.append(get_DeltaVirtual_Flows_NK(il_connect,p_il_connect,A,ilds_il_connect))
+    
+    print(por_virtual_ilds)
+    
+    por_connected=p_init
+    for i in range(len(ilds)):
+        por_connected-=A[ilds[i]]*por_virtual_ilds[i]
+        
+    return por_connected
+        
+    
+#a reccursive approximation without solving equations
+def get_Virtual_Flows_reccursion_approx_NK(por_init,A,idls,iter=10):
+    pil_init=[]
+    for idl in idls:
+        p_init=por_init[idl]+np.sum([por_init[idlj]*A[idlj][idl] for idlj in idls if idlj!=idl])
+        pil_init.append(p_init)
+
+    pil_virtual=pil_init
+    
+    coeff_iter=0
+    for i in range(iter):
+        #pl1_virtual+=p1_init*(A[idl1][idl2]**(i+1))*(A[idl2][idl1]**(i+1))
+        coeff_iter+=(A[idl1][idl2]**(i+1))*(A[idl2][idl1]**(i+1))
+    
+    pl1_virtual+=p1_init*coeff_iter
+    
+    p2_init=por_init[idl2]+por_init[idl1]*A[idl1][idl2]
+    pl2_virtual=p2_init
+    
+    #for i in range(iter):
+    #    pl2_virtual+=p2_init*(A[idl1][idl2]**(i+1))*(A[idl2][idl1]**(i+1))
+    
+    pl2_virtual+=p2_init*coeff_iter
+    
+    print(pl1_virtual)
+    print(pl2_virtual)
+    
+    por_virtual=por_init+A[idl1]*pl1_virtual+A[idl2]*pl2_virtual
+    
+    return por_virtual
+
+
+def get_Virtual_Flows_N2(por_init,A,idl1,idl2):
+    a=np.array([[A[idl1][idl1],A[idl2][idl1]],[A[idl1][idl2],A[idl2][idl2]]])
+    b=np.array([-por_init[idl1],-por_init[idl2]])
+    [pl1_virtual,pl2_virtual]=np.linalg.solve(a,b)
+    print(pl1_virtual)
+    print(pl2_virtual)
+    
+    por_virtual=por_init+A[idl1]*pl1_virtual+A[idl2]*pl2_virtual
+    
+    return por_virtual
+
+#a reccursive approximation without solving equations
+def get_Virtual_Flows_reccursion_approx_N2(por_init,A,idl1,idl2,iter=10):
+    p1_init=por_init[idl1]+por_init[idl2]*A[idl2][idl1]
+    pl1_virtual=p1_init
+    
+    coeff_iter=0
+    for i in range(iter):
+        #pl1_virtual+=p1_init*(A[idl1][idl2]**(i+1))*(A[idl2][idl1]**(i+1))
+        coeff_iter+=(A[idl1][idl2]**(i+1))*(A[idl2][idl1]**(i+1))
+    
+    pl1_virtual+=p1_init*coeff_iter
+    
+    p2_init=por_init[idl2]+por_init[idl1]*A[idl1][idl2]
+    pl2_virtual=p2_init
+    
+    #for i in range(iter):
+    #    pl2_virtual+=p2_init*(A[idl1][idl2]**(i+1))*(A[idl2][idl1]**(i+1))
+    
+    pl2_virtual+=p2_init*coeff_iter
+    
+    print(pl1_virtual)
+    print(pl2_virtual)
+    
+    por_virtual=por_init+A[idl1]*pl1_virtual+A[idl2]*pl2_virtual
+    
+    return por_virtual
+    
+
+#a generic version for n-K
+def get_Virtual_Flows_NK(por_init,A,ilds):
+    a=[]
+    for idl in ilds:
+        a_row=np.array([A[idlj][idl] for idlj in ilds])
+        a.append(a_row)
+
+    b=np.array([-por_init[idl] for idl in ilds])
+    pls_virtual=np.linalg.solve(a,b)
+    print(pls_virtual)
+    
+    por_virtual=por_init
+    for i in range(len(ilds)):
+        por_virtual+=A[ilds[i]]*pls_virtual[i]
+    
+    return por_virtual
+
+
+def get_A_idl1_virtual_line(A,ind_lor,ind_lex,sub_id):
+    A_idl1_virtual_line=[A[idl1][i] for i in range(env.n_line) if ind_lor[i]==sub_id]
+    A_idl1_virtual_line+=[-A[idl1][i] for i in range(env.n_line) if ind_lex[i]==sub_id]
+    print(A_idl1_virtual_line)
+    A_idl1_virtual_line=np.sum(A_idl1_virtual_line)
+    print(A_idl1_virtual_line)
+    
+    return A_idl1_virtual_line
+
+def get_Virtual_Flows_N1_topo(por_init,por_topo,A,idl1,A_topo,ind_lor,ind_lex,sub_id ):
+    A_idl1_virtual_line=get_A_idl1_virtual_line(A,ind_lor,ind_lex,sub_id)
+   
+    a=np.array([[A[idl1][idl1],A[idl2][idl1]],[A[idl1][idl2],A[idl2][idl2]]])
+    
+    a=np.array([[-1,A_topo[idl1]],[A_idl1_virtual_line,-1]])
+    b=np.array([-por_init[idl1],-por_topo])
+    #print(a)
+    #print(b)
+    [pl1_virtual,pl2_virtual]=np.linalg.solve(a,b)
+    print(pl1_virtual)
+    print(pl2_virtual)
+    
+    por_virtual=por_init+A[idl1]*pl1_virtual+A_topo*pl2_virtual
+    
+    return por_virtual
+

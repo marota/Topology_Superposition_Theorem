47ead565348e52fb5d5b7d0470d5c0db9ac63788
marota <antoine-marot@live.fr>
2023-06-14 17:47:38 +0200

    making the SuperpoTheorem_reconnect_using_delta_thetas more readable

--- superposition_theorem.py
+++ superposition_theorem.py
@@ -66,8 +66,6 @@ def get_Approx_Virtual_Flows_NK(por_init,A,idls,niter):
         pl_virtuals+=residuals
 
     return pl_virtuals #[28.426771, 7.6831803, 27.362656]
-
-get_Approx_Virtual_Flows_NK(por_init,A,ids_l,niter=10)
     
 #a reccursive approximation without solving equations
 def get_Virtual_Flows_reccursion_approx_NK(por_init,A,idls,iter=10):
@@ -183,4 +181,145 @@ def get_Virtual_Flows_N1_topo(por_init,por_topo,A,idl1,A_topo,ind_lor,ind_lex,su
     por_virtual=por_init+A[idl1]*pl1_virtual+A_topo*pl2_virtual
     
     return por_virtual
+
+def get_delta_theta_line(obs,id_line):
+    theta_or=obs.theta_or
+    theta_ex=obs.theta_ex
+
+    sub_l_ex=obs.line_ex_to_subid[id_line]
+    sub_l_or=obs.line_or_to_subid[id_line]
+    lines_sub_or_l=list(obs.get_obj_connect_to(substation_id=sub_l_or)['lines_or_id'])+list(obs.get_obj_connect_to(substation_id=sub_l_or)['lines_ex_id'])
+    lines_sub_ex_l=list(obs.get_obj_connect_to(substation_id=sub_l_ex)['lines_or_id'])+list(obs.get_obj_connect_to(substation_id=sub_l_ex)['lines_ex_id'])
+
+    #print(id_line)
+    #print(lines_sub_or_l)
+    #print(lines_sub_ex_l)
+    theta_or_l=0.
+    #for ids of thetas, cf previous section where we identified those
+    thetas_or_l=np.append(theta_or[obs.get_obj_connect_to(substation_id=sub_l_or)['lines_or_id']],
+                          theta_ex[obs.get_obj_connect_to(substation_id=sub_l_or)['lines_ex_id']])
+    thetas_or_l=thetas_or_l[thetas_or_l!=0]
+    #print(thetas_or_l)
+
+    if len(thetas_or_l)!=0:
+        if (np.sum(np.abs(thetas_or_l))!=0):
+            thetas_or_l=thetas_or_l[thetas_or_l!=0]
+        theta_or_l=np.median(thetas_or_l)
+    
+    theta_ex_l=0.
+    thetas_ex_l=np.append(theta_or[obs.get_obj_connect_to(substation_id=sub_l_ex)['lines_or_id']],
+                    theta_ex[obs.get_obj_connect_to(substation_id=sub_l_ex)['lines_ex_id']])
+    thetas_ex_l=thetas_ex_l[thetas_ex_l!=0]
+    #print(thetas_ex_l)
+    
+    if len(thetas_ex_l)!=0:
+        if (np.sum(np.abs(theta_ex_l))!=0):
+            theta_ex_l=theta_ex_l[theta_ex_l!=0]
+        theta_ex_l=np.median(thetas_ex_l)
+    delta_theta_l=theta_or_l-theta_ex_l
+    #delta_theta_l2=theta_or[8]-theta_ex[12]
+    #print(delta_theta_l)
+    
+    return delta_theta_l#/360*2*3.14159 #use PI number with enough significant digits!!
+
+def get_equivalent_delta_theta(obs,sub_or,sub_ex):
+    
+    #lines_sub_or_l3=list(obs.get_obj_connect_to(substation_id=sub_or)['lines_or_id'])+list(obs.get_obj_connect_to(substation_id=sub_or)['lines_ex_id'])
+    #lines_sub_ex_l3=list(obs.get_obj_connect_to(substation_id=sub_ex)['lines_or_id'])+list(obs.get_obj_connect_to(substation_id=sub_ex)['lines_ex_id'])
+    
+    theta_or_l=0
+    theta_ex_l=0
+    
+    theta_or=obs.theta_or
+    theta_ex=obs.theta_ex
+    
+    theta_or_l_sub=list(theta_or[list(obs.get_obj_connect_to(substation_id=sub_or)['lines_or_id'])])
+    theta_or_l_sub+=list(theta_ex[list(obs.get_obj_connect_to(substation_id=sub_or)['lines_ex_id'])])
+    
+    #print(theta_or_l_sub)
+    if 0.0 in theta_or_l_sub:
+        theta_or_l_sub.remove(0.0)
+    
+    if (len(theta_or_l_sub)>=1):
+        theta_or_l=np.median(theta_or_l_sub)
+    
+        
+    theta_ex_l_sub=list(theta_or[list(obs.get_obj_connect_to(substation_id=sub_ex)['lines_or_id'])])
+    theta_ex_l_sub+=list(theta_ex[list(obs.get_obj_connect_to(substation_id=sub_ex)['lines_ex_id'])])
+    
+    #print(theta_ex_l_sub)
+    if 0.0 in theta_ex_l_sub:
+        theta_ex_l_sub.remove(0.0)
+    if (len(theta_ex_l_sub)>=1):
+        theta_ex_l=np.median(theta_ex_l_sub)
+    
+    #print(theta_or_l)
+    #print(theta_ex_l)
+    
+    return (theta_or_l-theta_ex_l)#/360*2*3.14159
+
+
+def get_betas_coeff_N2_reconnect(id_l1,id_l2,env):
+    init_obs, *_ = env.simulate(env.action_space())
+    obs_disconnected_l1_l2, *_ = env.simulate(env.action_space({"set_line_status": [(id_l1, -1),(id_l2, -1)]}))
+    obs_disconnect_l1, *_ = env.simulate(env.action_space({"set_line_status": [(id_l1, -1)]}))
+    obs_disconnect_l2, *_ = env.simulate(env.action_space({"set_line_status": [(id_l2, -1)]}))
+    
+    expected_betas=[init_obs.p_or[id_l1]/obs_disconnect_l2.p_or[id_l1],init_obs.p_or[id_l2]/obs_disconnect_l1.p_or[id_l2]]
+    print("expected_betas :"+str(expected_betas))
+    
+    sub_l1_ex=init_obs.line_ex_to_subid[id_l1]
+    sub_l1_or=init_obs.line_or_to_subid[id_l1]
+
+    sub_l2_ex=init_obs.line_ex_to_subid[id_l2]
+    sub_l2_or=init_obs.line_or_to_subid[id_l2]
+    
+    a=[[1,1-get_delta_theta_line(obs_disconnect_l1,id_l1)/get_delta_theta_line(obs_disconnected_l1_l2,id_l1)],
+        [1-get_delta_theta_line(obs_disconnect_l2,id_l2)/get_delta_theta_line(obs_disconnected_l1_l2,id_l2),1]
+    ]
+    b=[1,1]
+
+    
+    print(a)
+    print(b)
+
+    betas=np.linalg.solve(a,b)
+    
+    print("computed betas: "+str(betas))
+    return betas
+    
+    
+#def get_betas_coeff_reconnect(id_l1,id_l2,env):
+def get_betas_coeff_N_reconnect(idls,obs_connect_idls,obs_start,obs_target=None):
+    
+    if obs_target is not None:
+        #expected_betas=[obs_target.p_or[id_l]/obs_connect_idls[i].p_or[id_l] for i,id_l in enumerate(idls)]
+        expected_betas=[get_delta_theta_line(obs_target,id_l)/get_delta_theta_line(obs_connect_idls[i],id_l) for i,id_l in enumerate(idls)]
+        print("expected_betas :"+str(expected_betas))
+    
+
+    n_lines_connect=len(idls)
+    a=np.zeros((n_lines_connect,n_lines_connect))
+    b=np.zeros(n_lines_connect)
+    
+    #    a=[[1,1-get_delta_theta_line(obs_disconnect_l1,id_l1)/get_delta_theta_line(obs_disconnected_l1_l2,id_l1)],
+    #    [1-get_delta_theta_line(obs_disconnect_l2,id_l2)/get_delta_theta_line(obs_disconnected_l1_l2,id_l2),1]
+    #]
+    a=np.array([[1-get_delta_theta_line(obs_connect_idls[i],idls[j])/get_delta_theta_line(obs_start,idls[j])\
+        for i in range(len(idls))] for j in range(len(idls))])
+    
+    np.fill_diagonal(a, 1)
+       
+    b=np.ones(n_lines_connect)
+    
+        
+    print(a)
+    print(b)
+
+    betas=np.linalg.solve(a,b)
+    
+    print("computed betas: "+str(betas))
+    return betas
+    
+    
 

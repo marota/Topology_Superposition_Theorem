4e2f5a93af99bfc784499a3d09f32434e516db8b
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2024-06-28 10:54:23 +0200

    before merge

--- test.py
+++ test.py
@@ -1,4 +1,88 @@
 import grid2op
+import numpy as np
+
+from superposition_theorem import State
+
+env = grid2op.make("l2rpn_case14_sandbox")
+param = env.parameters
+param.ENV_DC = True  # force the computation of the powerflow in DC mode
+param.MAX_LINE_STATUS_CHANGED = 99999
+param.MAX_SUB_CHANGED = 99999
+param.NO_OVERFLOW_DISCONNECTION = True
+env.change_parameters(param)
+env.change_forecast_parameters(param)
+env.set_id(0)
+obs = env.reset()
+start_obs, *_  = env.step(env.action_space({}))
+
+id_sub1 = 5  
+id_sub2 = 4  
+unitary_action_list = [{'set_bus': {'substations_id': [(5, (1, 1, 2, 2, 1, 2, 2))]}},  # sub5
+                       {'set_bus': {'substations_id': [(4, (2, 1, 2, 1, 2))]}},  # sub4
+                       ]
+
+unitary_actions = [env.action_space(unitary_act) for unitary_act in unitary_action_list]
+
+init_state = State.from_grid2op_obs(start_obs)
+
+# first unary action
+id_sub1 = 5
+act1 = init_state.get_emptyact()
+act1.set_subid(id_sub1)
+# act1.get_elem_sub()  # to know which elements are connected to this substation
+act1.set_bus(lines_id=[(7, 1), (8, 1), (9, 2), (17, 2)],
+             loads_id=[(4, 2)],
+             gens_id=[(2, 1), (3, 2)])
+
+# second unary action
+id_sub2 = 4
+act2 = init_state.get_emptyact()
+act2.set_subid(id_sub2)
+# act2.get_elem_sub()  # to know which elements are connected to this substation
+act2.set_bus(lines_id=[(1, 2), (4, 1), (6, 2), (17, 1)],
+             loads_id=[(3, 2)])
+# state = State.from_grid2op_obs(start_obs, subs_actions_unary=[act1, act2])
+# computed_por = state.compute_flows_node_split([act1, act2])
+# real_obs, *_ = start_obs.simulate(unitary_actions[0] + unitary_actions[1], time_step=0)
+# print(np.abs(computed_por - real_obs.p_or).max())
+#############""
+
+
+state = State.from_grid2op_obs(start_obs,
+                               line_ids_disc_unary=(2, 3),
+                               subs_actions_unary=[act1, act2])
+
+
+
+# p_or_combined = state.compute_flows(line_ids_disc=(2, 3), subs_actions=[act1, act2])
+
+# combined_act = act1.to_grid2op(env.action_space) + act2.to_grid2op(env.action_space)
+# combined_act += env.action_space({"set_line_status": [(2, -1), (3, -1)]})
+# obs_true, r, done, info = start_obs.simulate(combined_act, time_step=0)
+# assert not info["exception"], f'Error while performing the powerflow check: {info["exception"]}'
+# por_true = obs_true.p_or
+# print(f"Max difference: {np.abs(p_or_combined - por_true).max():.2e} MW")
+
+##########
+print("Normal A")
+state.compute_flows_disco_lines((2, 3))
+
+print("A I got")
+res, total_time, nb_cont  = state.compute_flows_n1(subs_actions=[act1, act2], line_ids=(2, 3))
+
+combined_act = act1.to_grid2op(env.action_space) + act2.to_grid2op(env.action_space)
+f_0 , *_ = start_obs.simulate(combined_act + env.action_space({"set_line_status": [(2, -1)]}), time_step=0)
+f_1 , *_ = start_obs.simulate(combined_act + env.action_space({"set_line_status": [(3, -1)]}), time_step=0)
+print(f"Max difference: {np.abs(res[0] - f_0.p_or).max():.2e} MW")
+print(f"Max difference: {np.abs(res[1] - f_1.p_or).max():.2e} MW")
+import pdb
+pdb.set_trace()
+import sys
+sys.exit(0)
+##############
+
+
+import grid2op
 from superposition_theorem import State
 
 env = grid2op.make("l2rpn_case14_sandbox")
